34c113533f5434c18fdaae59e7d4b4b3df3c6654
prem <premanand.achuthan@cimr.cam.ac.uk>
2015-11-05 16:58:56 +0000

    bug fix for issue pydgin-auth-56

--- pydgin_auth/permissions.py
+++ pydgin_auth/permissions.py
@@ -13,6 +13,9 @@ def get_authenticated_idx_and_idx_types(user=None, idx_keys=None, idx_type_keys=
     # get all public idx_keys and idx_type_keys
     (idx_keys_public, idx_type_keys_public) = elastic_factory.get_idx_and_idx_type_keys(auth_public=True)
 
+    # get all the private idx_keys and idx_type_keys
+    (idx_keys_private, idx_type_keys_private) = elastic_factory.get_idx_and_idx_type_keys(auth_public=False)
+
     # user is None...return all public keys
     if user is None:
         return idx_keys_public, idx_type_keys_public
@@ -20,28 +23,36 @@ def get_authenticated_idx_and_idx_types(user=None, idx_keys=None, idx_type_keys=
     idx_keys_auth = []
     idx_type_keys_auth = []
 
-    # idx_keys and idx_type_keys is None, so first fetch private keys
-    if idx_keys is None or idx_type_keys is None:
-        # First add all the public keys
-        idx_keys_auth.extend(idx_keys_public)
-        idx_type_keys_auth.extend(idx_type_keys_public)
-        (idx_keys_private, idx_type_keys_private) = elastic_factory.get_idx_and_idx_type_keys(auth_public=False)
+    # idx_keys or idx_type_keys is None, first fetch and add public keys
+    # if idx_keys is None or idx_type_keys is None:
 
-        if idx_keys is None:
-            idx_keys = idx_keys_private
-        if idx_type_keys is None:
-            idx_type_keys = idx_type_keys_private
+    if idx_keys is None:
+        # First add all the public idx keys
+        idx_keys_auth.extend(idx_keys_public)
+        # Assign the idx keys that need to be checked
+        idx_keys = idx_keys_private
     else:
-        # as idx_keys and idx_keys is not None, first add the idx_keys if it is in public keys
+        # don't add all, limit the one that the user has passed
         idx_keys_auth = [idx_key for idx_key in idx_keys if idx_key in idx_keys_public]
+
+    if idx_type_keys is None:
+        # First add all the public idx type keys
+        idx_type_keys_auth.extend(idx_type_keys_public)
+        # Assign the idx type keys that need to be checked
+        idx_type_keys = idx_type_keys_private
+    else:
+        # don't add all, limit the one that the user has passed
         idx_type_keys_auth = [idx_key for idx_key in idx_type_keys if idx_key in idx_type_keys_public]
 
+    # get elastic model names for the idx_keys and types
     (model_names_idx, model_names_idx_types) = elastic_factory.get_elastic_model_names(idx_keys=idx_keys,
                                                                                            idx_type_keys=idx_type_keys)  # @IgnorePep8
-
+    # check if the user has permissions to see the idx model
     model_names_idx_auth = _check_content_type_perms(model_names_idx, user)
+    # check if the user has permissions to see the idx type model
     model_names_idx_types_auth = _check_content_type_perms(model_names_idx_types, user)
 
+    # finally get the actual idx keys from model names and return them
     (idx_auth, idx_types_auth) = elastic_factory.get_keys_from_model_names(model_names_idx_auth,
                                                                            model_names_idx_types_auth)
 
